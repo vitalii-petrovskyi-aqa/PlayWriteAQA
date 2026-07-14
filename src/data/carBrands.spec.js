cd1655ce32edf3e05369a4bd70bc53496793572b
Vitalii Petrovskyi <vitalii.petrovskyi@mgid.com>
2026-07-14 07:37:54 +0300

    commit adding  API JSONPlaceholder

---
+++ src/data/carBrands.spec.js
@@ -0,0 +1,27 @@
+export const CAR_BRANDS=  {
+      "Audi": {
+          "id": 1,
+          "title": "Audi",
+          "logoFilename": "audi.png"
+      },
+      "BMW": {
+          "id": 2,
+          "title": "BMW",
+          "logoFilename": "bmw.png"
+      },
+      "Ford": {
+          "id": 3,
+          "title": "Ford",
+          "logoFilename": "ford.png"
+      },
+      "Porsche": {
+          "id": 4,
+          "title": "Porsche",
+          "logoFilename": "porsche.png"
+      },
+      "Fiat": {
+          "id": 5,
+          "title": "Fiat",
+          "logoFilename": "fiat.png"
+      }
+    }
--- tests/setup/loginAndSaveState.setup.js
+++ tests/setup/loginAndSaveState.setup.js
@@ -1,22 +1,34 @@
-
 import {expect, test as setup} from "@playwright/test";
 import {USERS} from "../../src/data/users.js";
 import WelcomePage from "../../src/pageObjects/welcomePage/WelcomePage.js";
 import {USER1_STORAGE_STATE_PATH} from "../../src/data/constants.js";
 
 
-setup(`Login as ${USERS.USER1.email} and save storage state`, async ({page})=>{
-    const welcomePage = new WelcomePage(page)
-    await welcomePage.navigate()
-    const signInPopup = await welcomePage.header.clickSignInButton()
-    await signInPopup.emailInput.fill(USERS.USER1.email);
-    await signInPopup.passwordInput.fill(USERS.USER1.password);
-    await signInPopup.loginBtn.click()
+setup(`Login as ${USERS.USER1.email} and save storage state`, async ({request})=>{
+//     const welcomePage = new WelcomePage(page)
+//     await welcomePage.navigate()
+//     const signInPopup = await welcomePage.header.clickSignInButton()
+//     await signInPopup.emailInput.fill(USERS.USER1.email);
+//     await signInPopup.passwordInput.fill(USERS.USER1.password);
+//     await signInPopup.loginBtn.click()
 
-    await expect(page).toHaveURL(/garage/)
+//     await expect(page).toHaveURL(/garage/)
 
-    //  save to file
-    await page.context().storageState({
+//     //  save to file
+//     await page.context().storageState({
+//         path: USER1_STORAGE_STATE_PATH
+//     })
+
+await request.post('/api/auth/signin', {
+        data: {
+            "email": USERS.USER1.email,
+            "password": USERS.USER1.password,
+            "remember": false
+        }
+    })
+
+    await request.storageState({
         path: USER1_STORAGE_STATE_PATH
     })
+
 })
